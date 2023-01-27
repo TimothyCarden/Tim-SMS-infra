f92d2c9148dd753d1e44ab932e7e9dc0ff2d555d
mnahapetyan@provectus.com <mher.nahapetyan@provectus.com>
2023-01-28 01:35:20 +0400

    add outputs

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,7 @@
-# output "infra" {
-#   value = module.infrastructure.vpc
-# }
+output "app_client_id" {
+  value = module.infrastructure.app_client_id
+}
+
+output "cognito_user_pool_id" {
+  value = module.infrastructure.cognito_user_pool_id
+}
