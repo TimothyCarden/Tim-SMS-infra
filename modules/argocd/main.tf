d666d5216c07905784a8256d77f07f875c09fdc7
mnahapetyan@provectus.com <mher.nahapetyan@provectus.com>
2023-05-27 19:45:56 +0200

    test argocd

---
+++ modules/argocd/main.tf
@@ -0,0 +1,34 @@
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = var.argocd_namespace
+  }
+}
+resource "helm_release" "argocd" {
+  depends_on = [kubernetes_namespace.argocd]
+  name       = var.release_name
+  chart      = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  namespace  = kubernetes_namespace.argocd.id
+  version    = var.chart_version
+  values = [<<EOF
+server:
+    extraArgs:
+        - --insecure
+        - --rootpath=/argo-cd
+    ingress:
+        enabled: true
+        pathType: Prefix
+        paths:
+            - /argo-cd
+        annotations:
+            kubernetes.io/ingress.class: "nginx"
+            nginx.ingress.kubernetes.io/ssl-redirect: "true"
+            nginx.ingress.kubernetes.io/rewrite-target: "/"
+            nginx.ingress.kubernetes.io/certificate: ${var.certificate_arn}
+        ingressClassName: ${var.ingress_class_name}
+        host: ${var.hostname}
+image:
+    tag: v2.6.7
+EOF
+  ]
+}
