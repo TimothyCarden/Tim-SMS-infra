0005d02dfb2d86f46331300232337f18abd7a387
mnahapetyan@provectus.com <mher.nahapetyan@provectus.com>
2023-05-28 14:18:42 +0200

    fix ingress class name

--- modules/argocd/main.tf
+++ modules/argocd/main.tf
@@ -3,31 +3,32 @@ resource "kubernetes_namespace" "argocd" {
     name = var.argocd_namespace
   }
 }
-# resource "helm_release" "argocd" {
-#   depends_on = [kubernetes_namespace.argocd]
-#   name       = var.release_name
-#   chart      = "argo-cd"
-#   repository = "https://argoproj.github.io/argo-helm"
-#   namespace  = kubernetes_namespace.argocd.id
-#   version    = var.chart_version
-#   values = [<<EOF
-# server:
-#     extraArgs:
-#         - --insecure
-#         - --rootpath=/argo-cd
-#     ingress:
-#         enabled: true
-#         pathType: Prefix
-#         paths:
-#             - /argo-cd
-#         annotations:
-#             nginx.ingress.kubernetes.io/ssl-redirect: "true"
-#             nginx.ingress.kubernetes.io/rewrite-target: "/"
-#             nginx.ingress.kubernetes.io/certificate: ${var.certificate_arn}
-#         ingressClassName: ${var.ingress_class_name}
-#         hosts: ["argo-cd.dev.actrusfm.com"]
-# image:
-#     tag: v2.6.7
-# EOF
-#   ]
-# }
+
+resource "helm_release" "argocd" {
+  depends_on = [kubernetes_namespace.argocd]
+  name       = var.release_name
+  chart      = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  namespace  = kubernetes_namespace.argocd.id
+  version    = var.chart_version
+  values = [<<EOF
+server:
+    extraArgs:
+        - --insecure
+        - --rootpath=/argo-cd
+    ingress:
+        enabled: true
+        pathType: Prefix
+        paths:
+            - /argo-cd
+        annotations:
+            nginx.ingress.kubernetes.io/ssl-redirect: "true"
+            nginx.ingress.kubernetes.io/rewrite-target: "/"
+            nginx.ingress.kubernetes.io/certificate: ${var.certificate_arn}
+        ingressClassName: ${var.ingress_class_name}
+        hosts: ["argo-cd.dev.actrusfm.com"]
+image:
+    tag: v2.6.7
+EOF
+  ]
+}
