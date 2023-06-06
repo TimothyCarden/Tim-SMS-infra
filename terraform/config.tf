62941eb5faac5c21de5b0bd537d7f6882ed3801f
mnahapetyan@provectus.com <mher.nahapetyan@provectus.com>
2023-06-06 10:57:24 +0200

    test providers

--- terraform/config.tf
+++ terraform/config.tf
@@ -10,6 +10,15 @@ terraform {
       source  = "hashicorp/helm"
       version = "2.5.1"
     }
+
+    external = {
+      source  = "hashicorp/external"
+      version = "2.3.1"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.4.0"
+    }
   }
 
 
