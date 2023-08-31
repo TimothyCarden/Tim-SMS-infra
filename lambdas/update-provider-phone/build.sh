a0f9e158b5eb32ed11cd3ac4c0590f7dbd3cf8f1
mher1997 <mher.nahapetyan@provectus.com>
2023-08-31 13:46:30 +0200

    fix script

--- lambdas/update-provider-phone/build.sh
+++ lambdas/update-provider-phone/build.sh
@@ -2,7 +2,7 @@
 
 pip3 install --target ./package -r requirements.txt
 cd package || exit
-zip -r ../update-provider-phone.zip .
+zip -r ../update-provider-phone-package.zip .
 cd ../
-zip -g update-provider-phone.zip lambda_function.py
+zip -g update-provider-phone-package.zip lambda_function.py
 rm -R ./package
